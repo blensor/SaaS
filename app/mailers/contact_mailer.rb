7849c1f50208e965f7c34a37ce61a46c45486c8f
Brian L Ensor <b.ensor@gmail.com>
2015-12-15 19:19:45 +0000

    Modified contact_mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'your@email_address.com'
+  default to: 'b.ensor@gmail'
   def contact_email(name, email, body)
     @name = name
     @email = email
