0c848844a50e17f97409a93ee0416ac1fc25408b
Brian L Ensor <b.ensor@gmail.com>
2015-12-18 20:48:23 +0000

    Fixed location of contact_email.html.erb...thus error was fixed

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,11 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'b.ensor@gmail'
+  default to: 'b.ensor@gmail.com'
   def contact_email(name, email, body)
+    
     @name = name
     @email = email
     @body = body
-    mail(from: email, subject: 'Contact has been made!')
+    
+    mail(from: email, subject: 'Contact form message')
   end
 end
